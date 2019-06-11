5becc80969173b06251d101e35d22c22d8631539
Aaron Hernandez <Aaron726@Aarons-MacBook-Air.local>
2019-06-10 23:36:00 -0600

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,28 @@
 console.log("Up and running!");
 
-var cards = ["queen", "queen", "king", "kind",];
 var cardsInPlay = [];
+var cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png",
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/quen-of-diamonds.png",
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png",
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage:"images/king-of-diamonds.png",
+}
+];
 
 function checkForMatch(){
 	if (cardsInPlay[0] === cardsInPlay[1]) {
@@ -13,8 +34,11 @@ function checkForMatch(){
 
 function flipCard(cardId){	
 
-console.log("User flipped " + cards[cardId]);	
-cardsInPlay.push(cards[cardId]);
+console.log("User flipped " + cards[cardId].rank);
+console.log(cards[cardId].suit);
+console.log(cards[cardId].cardImage);
+cardsInPlay.push(cards[cardId].rank);
+
 
 };
 
